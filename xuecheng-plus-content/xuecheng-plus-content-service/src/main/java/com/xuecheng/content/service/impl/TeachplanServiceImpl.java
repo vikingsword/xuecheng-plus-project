17961c64b079807341de4c00f4717be8cac6505a
vikingar <423141759@qq.com>
2023-04-11 20:56:02 +0800

    删除课程

--- xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/service/impl/TeachplanServiceImpl.java
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/service/impl/TeachplanServiceImpl.java
@@ -2,10 +2,13 @@ package com.xuecheng.content.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.xuecheng.content.mapper.TeachplanMapper;
+import com.xuecheng.content.mapper.TeachplanMediaMapper;
 import com.xuecheng.content.model.dto.SaveTeachplanDto;
 import com.xuecheng.content.model.dto.TeachplanDto;
 import com.xuecheng.content.model.po.Teachplan;
+import com.xuecheng.content.model.po.TeachplanMedia;
 import com.xuecheng.content.service.TeachplanService;
+import com.xuecheng.exception.XueChengPlusException;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,6 +21,9 @@ public class TeachplanServiceImpl implements TeachplanService {
 
     @Autowired
     TeachplanMapper teachplanMapper;
+
+    @Autowired
+    TeachplanMediaMapper teachplanMediaMapper;
 
     @Override
     public List<TeachplanDto> findTeachplanTree(long courseId) {
@@ -50,11 +56,24 @@ public class TeachplanServiceImpl implements TeachplanService {
 
     }
 
+    @Transactional
     @Override
     public void deleteTeachplan(Long id) {
         Teachplan teachplan = teachplanMapper.selectById(id);
-        if (teachplan.getParentid() == 0) {
-            //
+        // parentId != 0 or (parentId=0 and 以这个id作为parentId进行查询，找不到内容(也就是没有子节点))
+        LambdaQueryWrapper<Teachplan> wrapper = new LambdaQueryWrapper<>();
+        wrapper.eq(Teachplan::getParentid, id);
+        List<Teachplan> teachplanList = teachplanMapper.selectList(wrapper);
+        if (teachplan.getParentid() != 0 || (teachplanList.size() == 0)) {
+            // 可以删除，物理删除
+            teachplanMapper.deleteById(id);
+            // 同时物理删除 media 中的信息
+            LambdaQueryWrapper<TeachplanMedia> mediaWrapper = new LambdaQueryWrapper<>();
+            mediaWrapper.eq(TeachplanMedia::getTeachplanId, id);
+            teachplanMediaMapper.delete(mediaWrapper);
+        } else {
+            // 不能删除，返回自定义异常 todo 返回errCode
+            XueChengPlusException.cast( "课程计划信息还有子级信息，无法操作");
         }
     }
 
