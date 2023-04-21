a8e9fda612e8c59fefd2b2ad8a473eccf4932be3
vikingar <423141759@qq.com>
2023-04-21 18:00:22 +0800

    提交审核

--- xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/service/CoursePublishService.java
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/service/CoursePublishService.java
@@ -3,22 +3,31 @@ package com.xuecheng.content.service;
 import com.xuecheng.content.model.dto.CoursePreviewDto;
 
 /**
- * @description 课程预览、发布接口
  * @author Mr.M
- * @date 2022/9/16 14:59
  * @version 1.0
+ * @description 课程预览、发布接口
+ * @date 2022/9/16 14:59
  */
 public interface CoursePublishService {
 
 
- /**
-  * @description 获取课程预览信息
-  * @param courseId 课程id
-  * @return com.xuecheng.content.model.dto.CoursePreviewDto
-  * @author Mr.M
-  * @date 2022/9/16 15:36
- */
-   public CoursePreviewDto getCoursePreviewInfo(Long courseId);
+    /**
+     * @param courseId 课程id
+     * @return com.xuecheng.content.model.dto.CoursePreviewDto
+     * @description 获取课程预览信息
+     * @author Mr.M
+     * @date 2022/9/16 15:36
+     */
+    CoursePreviewDto getCoursePreviewInfo(Long courseId);
 
+
+    /**
+     * @param courseId 课程id
+     * @return void
+     * @description 提交审核
+     * @author Mr.M
+     * @date 2022/9/18 10:31
+     */
+    void commitAudit(Long companyId, Long courseId);
 
 }
