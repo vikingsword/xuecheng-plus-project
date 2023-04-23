6dc5bf5dbdf559759221c457f629e514c06c6431
vikingar <423141759@qq.com>
2023-04-23 14:22:24 +0800

    微信扫码登陆

--- xuecheng-plus-auth/src/main/java/com/xuecheng/AuthApplication.java
+++ xuecheng-plus-auth/src/main/java/com/xuecheng/AuthApplication.java
@@ -3,13 +3,23 @@ package com.xuecheng;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.OkHttp3ClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
 
-@EnableFeignClients(basePackages={"com.xuecheng.*.feignclient"})
+@EnableFeignClients(basePackages = {"com.xuecheng.*.feignclient"})
 @SpringBootApplication
 public class AuthApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AuthApplication.class, args);
+    }
+
+    // 如果使用第三方远程调用，不在一个网关，则一定要用 RestTemplate
+    @Bean
+    RestTemplate restTemplate() {
+        RestTemplate restTemplate = new RestTemplate(new OkHttp3ClientHttpRequestFactory());
+        return restTemplate;
     }
 
 }
