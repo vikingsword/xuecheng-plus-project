f98e795a32b023d5238de4c2cc8c4f487de17758
vikingar <423141759@qq.com>
2023-04-27 20:00:07 +0800

    接收支付通知

--- xuecheng-plus-orders/xuecheng-plus-orders-api/src/main/java/com/xuecheng/orders/api/OrderController.java
+++ xuecheng-plus-orders/xuecheng-plus-orders-api/src/main/java/com/xuecheng/orders/api/OrderController.java
@@ -3,11 +3,13 @@ package com.xuecheng.orders.api;
 import com.alipay.api.AlipayApiException;
 import com.alipay.api.AlipayClient;
 import com.alipay.api.DefaultAlipayClient;
+import com.alipay.api.internal.util.AlipaySignature;
 import com.alipay.api.request.AlipayTradeWapPayRequest;
 import com.xuecheng.exception.XueChengPlusException;
 import com.xuecheng.orders.config.AlipayConfig;
 import com.xuecheng.orders.model.dto.AddOrderDto;
 import com.xuecheng.orders.model.dto.PayRecordDto;
+import com.xuecheng.orders.model.dto.PayStatusDto;
 import com.xuecheng.orders.model.po.XcPayRecord;
 import com.xuecheng.orders.service.OrderService;
 import com.xuecheng.orders.util.SecurityUtil;
@@ -22,8 +24,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 @Api(value = "订单支付接口", tags = "订单支付接口")
 @Slf4j
@@ -94,13 +101,63 @@ public class OrderController {
     }
 
 
-
     @ApiOperation("查询支付结果")
     @GetMapping("/payresult")
     @ResponseBody
-    public PayRecordDto payresult(String payNo) throws IOException {
+    public PayRecordDto payResult(String payNo) throws IOException {
         //调用支付宝接口查询
         PayRecordDto payRecordDto = orderService.queryPayResult(payNo);
         return payRecordDto;
+    }
+
+
+    @ApiOperation("接收支付结果通知")
+    @PostMapping("/receivenotify")
+    public void receiveNotify(HttpServletRequest request) throws UnsupportedEncodingException, AlipayApiException {
+        Map<String, String> params = new HashMap<>();
+        Map requestParams = request.getParameterMap();
+        for (Iterator iter = requestParams.keySet().iterator(); iter.hasNext(); ) {
+            String name = (String) iter.next();
+            String[] values = (String[]) requestParams.get(name);
+            String valueStr = "";
+            for (int i = 0; i < values.length; i++) {
+                valueStr = (i == values.length - 1) ? valueStr + values[i]
+                        : valueStr + values[i] + ",";
+            }
+            params.put(name, valueStr);
+        }
+
+        //验签
+        boolean verify_result = AlipaySignature.rsaCheckV1(params, ALIPAY_PUBLIC_KEY, AlipayConfig.CHARSET, "RSA2");
+
+        if (verify_result) {//验证成功
+
+            //商户订单号
+            String out_trade_no = new String(request.getParameter("out_trade_no").getBytes("ISO-8859-1"), "UTF-8");
+            //支付宝交易号
+            String trade_no = new String(request.getParameter("trade_no").getBytes("ISO-8859-1"), "UTF-8");
+            //交易状态
+            String trade_status = new String(request.getParameter("trade_status").getBytes("ISO-8859-1"), "UTF-8");
+            //appid
+            String app_id = new String(request.getParameter("app_id").getBytes("ISO-8859-1"), "UTF-8");
+            //total_amount
+            String total_amount = new String(request.getParameter("total_amount").getBytes("ISO-8859-1"), "UTF-8");
+
+            //交易成功处理
+            if (trade_status.equals("TRADE_SUCCESS")) {
+
+                PayStatusDto payStatusDto = new PayStatusDto();
+                payStatusDto.setOut_trade_no(out_trade_no);
+                payStatusDto.setTrade_status(trade_status);
+                payStatusDto.setApp_id(app_id);
+                payStatusDto.setTrade_no(trade_no);
+                payStatusDto.setTotal_amount(total_amount);
+
+                //处理逻辑。。。
+                orderService.saveAliPayStatus(payStatusDto);
+            }
+        }
+
+
     }
 }
