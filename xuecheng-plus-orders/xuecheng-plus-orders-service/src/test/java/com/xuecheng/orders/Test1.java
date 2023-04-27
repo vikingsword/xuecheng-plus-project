26b50a9ecaa5b827bef641f5001eb385bc10a04e
vikingar <423141759@qq.com>
2023-04-27 13:25:46 +0800

    支付结果通知

--- xuecheng-plus-orders/xuecheng-plus-orders-service/src/test/java/com/xuecheng/orders/Test1.java
+++ xuecheng-plus-orders/xuecheng-plus-orders-service/src/test/java/com/xuecheng/orders/Test1.java
@@ -1,22 +1,21 @@
 package com.xuecheng.orders;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 /**
- * @description TODO
  * @author Mr.M
- * @date 2022/10/2 10:32
  * @version 1.0
+ * @description TODO
+ * @date 2022/10/2 10:32
  */
- @SpringBootTest
+@SpringBootTest
 public class Test1 {
 
 
-  @Test
- public void test(){
+    @Test
+    public void test() {
 
-  }
+    }
 
 }
